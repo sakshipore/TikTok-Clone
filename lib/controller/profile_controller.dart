ba5b2c5846443f97e6681850588d77b4b5b693f2
sakshipore <88977771+sakshipore@users.noreply.github.com>
2022-11-10 01:29:00 +0530

    profile controller

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,10 +1,84 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
-  Rx<String> _uid="".obs;
+  Rx<String> _uid = "".obs;
+  final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
+  Map<String, dynamic> get user => _user.value;
+
+  updateUserId(String uid) {
+    _uid.value = uid;
+    getUserData();
+  }
+
   getUserData() async {
     List<String> thumbnails = [];
-    var myVideos=await FirebaseFirestore.instance.collection("videos").where("uid", isEqualTo: _uid.value).get();
+    var myVideos = await FirebaseFirestore.instance
+        .collection("videos")
+        .where("uid", isEqualTo: _uid.value)
+        .get();
+
+    for (int i = 0; i < myVideos.docs.length; i++) {
+      thumbnails.add((myVideos.docs[i].data() as dynamic)['thumbnail']);
+    }
+
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(_uid.value)
+        .get();
+
+    final userData = userDoc.data() as dynamic;
+    String name = userDoc['name'];
+    String profilePic = userDoc['profilePic'];
+    int likes = 0;
+    int following = 0;
+    int followers = 0;
+    bool isFollowing = false;
+
+    for (var item in myVideos.docs) {
+      likes += (item.data()['likes'] as List).length;
+    }
+
+    var followerDoc = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(_uid.value)
+        .collection("followers")
+        .get();
+
+    var followingDoc = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(_uid.value)
+        .collection("following")
+        .get();
+
+    followers = followerDoc.docs.length;
+    following = followingDoc.docs.length;
+
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(_uid.value)
+        .collection("followers")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((value) {
+      if (value.exists) {
+        isFollowing = true;
+      } else {
+        isFollowing = false;
+      }
+    });
+
+    _user.value = {
+      'followers': followers.toString(),
+      'following': following.toString(),
+      'likes': likes.toString(),
+      'profilePic': profilePic,
+      'name': name,
+      'thumbnails': thumbnails,
+      'isFollowing':isFollowing,
+    };
+
+    update();
   }
 }
--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -1,10 +1,26 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:tik_tok_clone/controller/profile_controller.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   String uid;
   ProfileScreen({Key? key, required this.uid}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final ProfileController profileController = Get.put(ProfileController());
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    profileController.updateUserId(widget.uid);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,151 +34,157 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: GetBuilder<ProfileController>(
+        init: ProfileController(),
+        builder: (controller) {
+          return SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
                 children: [
-                  ClipOval(
-                    child: CachedNetworkImage(
-                      imageUrl: '',
-                      fit: BoxFit.contain,
-                      height: 100.h,
-                      width: 100.w,
-                      placeholder: (context, url) => CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(
-                        Icons.error,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipOval(
+                        child: CachedNetworkImage(
+                          imageUrl: controller.user['profilePic'],
+                          fit: BoxFit.contain,
+                          height: 100.h,
+                          width: 100.w,
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => Icon(
+                            Icons.error,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            controller.user['followers'],
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "Followers",
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 25.w,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            controller.user["following"],
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "Following",
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 25.w,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            controller.user["likes"],
+                            style: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.h,
+                          ),
+                          Text(
+                            "Likes",
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 35.h,
+                  ),
+                  Container(
+                    width: 150.w,
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black12,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Sign Out",
                       ),
                     ),
                   ),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  // GridView.builder(
+                  //   shrinkWrap: true,
+                  //   physics: NeverScrollableScrollPhysics(),
+                  //   gridDelegate: SilverGridDelegateWithFixedCrossAxisCount(
+                  //     crossAxisCount: 2,
+                  //     childAspectRatio: 1,
+                  //     crossAxisSpacing: 1,
+                  //   ),
+                  //   itemCount: 23,
+                  //   itemBuilder: (context, index) {
+                  //     return CachedNetworkImage(
+                  //       imageUrl: '',
+                  //       errorWidget: (context, url, error) => Icon(
+                  //         Icons.error,
+                  //       ),
+                  //     );
+                  //   },
+                  // ),
                 ],
               ),
-              SizedBox(
-                height: 20.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "10",
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Text(
-                        "Followers",
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 25.w,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "10",
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Text(
-                        "Followers",
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 25.w,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "10",
-                        style: TextStyle(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                      Text(
-                        "Followers",
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 35.h,
-              ),
-              Container(
-                width: 150.w,
-                height: 50.h,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black12,
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    "Sign Out",
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10.h,
-              ),
-              // GridView.builder(
-              //   shrinkWrap: true,
-              //   physics: NeverScrollableScrollPhysics(),
-              //   gridDelegate: SilverGridDelegateWithFixedCrossAxisCount(
-              //     crossAxisCount: 2,
-              //     childAspectRatio: 1,
-              //     crossAxisSpacing: 1,
-              //   ),
-              //   itemCount: 23,
-              //   itemBuilder: (context, index) {
-              //     return CachedNetworkImage(
-              //       imageUrl: '',
-              //       errorWidget: (context, url, error) => Icon(
-              //         Icons.error,
-              //       ),
-              //     );
-              //   },
-              // ),
-            ],
-          ),
-        ),
+            ),
+          );
+        }
       ),
     );
   }
